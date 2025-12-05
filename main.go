9f3dfe27f216dfc48e2cad7a098154fe6ee65957
jaharbaugh <harbaugh.ja@gmail.com>
2025-12-05 09:50:56 -0600

    Added metrics and reset functions

--- main.go
+++ main.go
@@ -2,16 +2,26 @@ package main
 
 import(
 	"net/http"
+	"fmt"
+	"sync/atomic"
+	"log"
 )
 
-
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
 
 func main(){
 
 	multiplexer := http.NewServeMux()
+	
+	cfg:=  &apiConfig{}
+	cfg.fileServerHits.Store(0)
 
-	multiplexer.HandleFunc("/healthz", handler)
-
+	multiplexer.HandleFunc("GET /healthz", handlerHealthz)
+	multiplexer.HandleFunc("GET /metrics", cfg.handlerMetrics )
+	multiplexer.HandleFunc("POST /reset", cfg.handlerReset)
+	
 	var server http.Server
 	server.Handler = multiplexer
 	server.Addr = ":8080"
@@ -19,14 +29,34 @@ func main(){
 	dir := http.Dir(".")
 	fileServer := http.FileServer(dir)
 	strippedFileServer := http.StripPrefix("/app", fileServer)
-	multiplexer.Handle("/app/", strippedFileServer)
+	multiplexer.Handle("/app/", cfg.middlewareMetricsInc(strippedFileServer))
 
-	server.ListenAndServe()
+	log.Fatal(server.ListenAndServe())
 
 }
 
-func handler (w http.ResponseWriter, req *http.Request){ 
+func handlerHealthz (w http.ResponseWriter, req *http.Request){ 
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) handlerReset (w http.ResponseWriter, req *http.Request){
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileServerHits.Store(0)
+
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request){
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %v\n", cfg.fileServerHits.Load())))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
