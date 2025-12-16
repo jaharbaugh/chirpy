bbde70a87f47694d9cb247b01b4c73be5ad7cbe2
jaharbaugh <harbaugh.ja@gmail.com>
2025-12-16 13:14:41 -0600

    Added sorting to chirp endpoint

--- getChirps.go
+++ getChirps.go
@@ -3,7 +3,8 @@ package main
 import(
 	"net/http"
 	"log"
-	
+	"github.com/google/uuid"
+	"sort"
 )
 
 
@@ -12,6 +13,49 @@ func (cfg *apiConfig) handlerGetChirps (w http.ResponseWriter, req *http.Request
         w.WriteHeader(http.StatusMethodNotAllowed)
         return
     }
+
+	idStr := req.URL.Query().Get("author_id")
+		
+	if idStr != "" {
+
+		userID, err := uuid.Parse(idStr)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Could not parse author ID", err)
+			return
+		}
+
+		userChirps, err := cfg.db.GetChirpsByUserID(req.Context(), userID)
+		if err != nil{
+			log.Printf("Could not fetch User's chirps error: %v", err)
+			respondWithError(w, http.StatusInternalServerError, "Error fetching chirps", err)
+			return	
+		}
+
+		chirps := []Chirp{}
+		for _, dbChirp := range userChirps {
+    		chirps = append(chirps, Chirp{
+        	ID:        dbChirp.ID,
+        	CreatedAt: dbChirp.CreatedAt,
+        	UpdatedAt: dbChirp.UpdatedAt,
+        	Body:      dbChirp.Body,
+        	UserID:    dbChirp.UserID,
+    		})
+		}
+
+			sortString := req.URL.Query().Get("sort")
+			if sortString == "asc" || sortString == ""{
+				sort.Slice(chirps, func(i, j int) bool {
+					return chirps[i].CreatedAt.Before(chirps[j].CreatedAt)
+				})
+			}else if sortString == "desc"{
+			sort.Slice(chirps, func(i, j int) bool {
+				return chirps[i].CreatedAt.After(chirps[j].CreatedAt)
+			})
+		}
+
+		respondJSON(w, http.StatusOK, chirps)
+		return
+	}
 
 	allChirps, err := cfg.db.GetChirps(req.Context())
 	if err != nil{
@@ -29,6 +73,17 @@ func (cfg *apiConfig) handlerGetChirps (w http.ResponseWriter, req *http.Request
         Body:      dbChirp.Body,
         UserID:    dbChirp.UserID,
     	})
+	}
+
+	sortString := req.URL.Query().Get("sort")
+	if sortString == "asc" || sortString == ""{
+		sort.Slice(chirps, func(i, j int) bool {
+			return chirps[i].CreatedAt.Before(chirps[j].CreatedAt)
+		})
+	}else if sortString == "desc"{
+		sort.Slice(chirps, func(i, j int) bool {
+			return chirps[i].CreatedAt.After(chirps[j].CreatedAt)
+		})
 	}
 
 	respondJSON(w, http.StatusOK, chirps)
---
+++ sql/queries/getChirpsByUserID.sql
@@ -0,0 +1,5 @@
+-- name: GetChirpsByUserID :many
+
+SELECT * FROM chirps
+WHERE user_id = $1;
+
